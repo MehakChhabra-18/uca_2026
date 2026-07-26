78cf6e64a5812530ae3730e1b3e7922a6b92652b
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-07-26 18:50:09 +0530

    finding number of reverse pairs

---
+++ significantReversePairs.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+#define MAX 100000
+
+long long merge(int arr[], int temp[], int low, int mid, int high)
+{
+    long long count = 0;
+    int i, j, k;
+
+    // Count significant reverse pairs
+    j = mid + 1;
+    for (i = low; i <= mid; i++)
+    {
+        while (j <= high && (long long)arr[i] > 2LL * arr[j])
+            j++;
+
+        count += (j - (mid + 1));
+    }
+
+    // Merge two sorted halves
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= high)
+        temp[k++] = arr[j++];
+
+    for (i = low; i <= high; i++)
+        arr[i] = temp[i];
+
+    return count;
+}
+
+long long mergeSort(int arr[], int temp[], int low, int high)
+{
+    if (low >= high)
+        return 0;
+
+    int mid = (low + high) / 2;
+
+    long long count = 0;
+
+    count += mergeSort(arr, temp, low, mid);
+    count += mergeSort(arr, temp, mid + 1, high);
+    count += merge(arr, temp, low, mid, high);
+
+    return count;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[MAX];
+    int temp[MAX];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("%lld\n", mergeSort(arr, temp, 0, n - 1));
+
+    return 0;
+}
