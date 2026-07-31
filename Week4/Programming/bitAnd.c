e10f05e437744d06f09c0aaa4c935d405c80550d
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-07-31 10:49:47 +0530

    bitwise and implemented

---
+++ Week4/Programming/bitAnd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int bitAnd(int x, int y) {
+    return ~(~x | ~y);
+}
+
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("%d\n",bitAnd(x,y));
+    return 0;
+}
