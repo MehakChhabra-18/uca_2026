f662cded3f72222e0535f32d305d961dbf7daf5b
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-08-03 20:38:36 +0530

    fit bits implemented

---
+++ Week4/Programming/fitBits.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int fitsBits(int x, int n) {
+    int shift = 32 + (~n + 1);   // 32 - n
+    return !((((x << shift) >> shift) ^ x));
+}
+
+int main()
+{
+    int x,n;
+    scanf("%d %d", &x, &n);
+    printf("%d\n", fitsBits(x,n));
+}
