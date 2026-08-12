90575b0fd27e57a619d3e93d272e37ed3b1f4424
MehakChhabra-18 <mehakchhabra751@gmail.com>
2026-08-12 13:59:21 +0530

    updated code

--- Week5/Programming/base64Encoding.c
+++ Week5/Programming/base64Encoding.c
@@ -1,39 +1,31 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-void base64(char* str) {
-  char chartable[] = {
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-"};
-  int len = strlen(str);
-  for (int i = 0; i < len; i += 3) {
-    int firstchar = str[i];
-    int secondchar = (i + 1) < len ? str[i + 1] : 0;
-    int thirdchar = (i + 2) < len ? str[i + 2] : 0;
+void encode(char str[])
+{
+    char base64[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    int len=strlen(str);
+    for(int i=0;i<len;i+=3)
+    {
+      int a=str[i];
+      int b=(i+1)<len?str[i+1]:0;
+      int c=(i+2)<len?str[i+2]:0;
+      int n=(a<<16)|(b<<8)|c;
+      putchar(base64[n>>18]);
+      putchar(base64[(n>>12)&63]);
+      if((i+1)<len) putchar(base64[(n>>6)&63]);
+      else putchar('=');
 
-    int first64 = firstchar >> 2;
-    int second64 = ((firstchar & 3) << 4) | (secondchar >> 4);
-    int third64 = ((secondchar & 15) << 2) | (thirdchar >> 6);
-    int fourth64 = thirdchar & 63;
-
-    printf("%c", chartable[first64]);
-    printf("%c", chartable[second64]);
-
-    if ((i + 1) < len) {
-      printf("%c", chartable[third64]);
-    } else {
-      printf("=");
+      if(i+2<len) putchar(base64[n&63]);
+      else putchar('=');
     }
-    if ((i + 2) < len) {
-      printf("%c", chartable[fourth64]);
-    } else {
-      printf("=");
-    }
-  }
 }
 
-int main() {
-  char str[100];
-  scanf("%s", str);
-  base64(str);
-  return 0;
+int main()
+{
+     char str[]="Man";
+     encode(str);
+
 }
+
+
